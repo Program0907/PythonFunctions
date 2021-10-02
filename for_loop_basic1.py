85c26e8705ac4a4703ad8eae049483a7cfb6757d
Program0907 <89719288+Program0907@users.noreply.github.com>
2021-10-01 18:26:28 -0700

    Add files via upload

---
+++ for_loop_basic1.py
@@ -0,0 +1,45 @@
+# I commented out all my code so that all programs don't run at the same time
+
+#x = 0
+#while x <= 150:
+#    print("looping - ", x)
+#    x += 1
+
+#for i in range (150):
+#    print(i)
+
+for x in range (5,1005, 5):
+    print(x)
+
+#x = 5
+#    if x > 100:
+#    	print(x)
+#   elif x = 50:
+#    	print("Coding")
+#    else:
+#    	print("Coding Dojo")
+#for i in range (1,105):
+#    if i % 10 == 0:
+#        print("Coding Dojo")
+#    elif i % 5 == 0: 
+#        print("Coding")
+#    else: 
+#        print(i)
+    
+#start = 1
+#end = 500000
+#num_list = range(start, end +1, 2)
+#final_sum = sum(num_list)
+#print("Final Sum=", final_sum)
+
+#count = 2018
+#while count >= 0:
+#    print('looping -' ,count)
+#    count -=4
+
+#lowNum = 2
+#highNum = 9 
+#mult = 3
+#for i in range(lowNum, highNum + 1):
+#    if i % mult == 0:
+#        print(i)
